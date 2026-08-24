8368c3d65df591c9ea245c9a2cc88a2d8ace80db
kundcoffee <kundcoffee@gmail.com>
2026-08-24 13:13:16 +0545

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Raj Biryani | Biryani in Kathmandu",
+  description: "Raj Biryani – serving authentic and delicious biryani in Kathmandu.",
+  keywords: [
+    "Raj Biryani",
+    "Biryani in Kathmandu",
+    "Best Biryani in Kathmandu",
+    "Chicken Biryani Kathmandu",
+    "Mutton Biryani Kathmandu",
+    "Biryani Restaurant Kathmandu",
+    "Kathmandu Biryani",
+    "Best Restaurant in Kathmandu"
+  ],
+
+  
+  icons: {
+    icon: [
+      {
+        url: "/Logo.jpeg",
+        href: "/Logo.jpeg",
+      },
+    ],
+    shortcut: "/Logo.jpeg",
+    apple: "/Logo.jpeg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} font-sans antialiased bg-white text-zinc-900`}
+      >
+
+            {children}
+            <Toaster position="top-right" richColors duration={2000} />
+
+      </body>
+    </html>
+  );
+}
